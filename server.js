2fcd4b0ac5156db138fced94bc617a13ffcfff7d
Jay Kushwaha <147177490+jayXkush@users.noreply.github.com>
2025-07-07 20:11:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // CORS setup for development and production
 app.use(cors({
